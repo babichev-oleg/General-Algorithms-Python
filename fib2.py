767918d38e4c35981e947acad13826a3230dc185
babichev-oleg <babichev-oleg@mail.ru>
2019-10-22 10:46:07 +0300

    Update fib2.py

--- fib2.py
+++ fib2.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3.6
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+if __name__ == "__main__":
+    print(fibonacci(10))
 
