c77b0002f7d0b8f45cad865bf3b1c2fbcf5b472c
babichev-oleg <babichev-oleg@mail.ru>
2019-10-22 20:51:01 +0300

    Update fib2.py

--- fib2.py
+++ fib2.py
@@ -1,8 +1,11 @@
-#!/usr/bin/python3.6
+!/usr/bin/python3.7
+import time
+start_time = time.time()
 def fibonacci(n):
     if n in (1, 2):
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 if __name__ == "__main__":
-    print(fibonacci(10))
+    print(fibonacci(35))
+print("--- %s seconds ---" % (time.time() - start_time))
 
