b136d615d9e53371e111c40158d72b5d4edc2f15
babichev-oleg <babichev-oleg@mail.ru>
2019-10-26 15:22:58 +0300

    Update fib6.py

--- fib6.py
+++ fib6.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3.7
+from typing import Generator
+import time
+start_time=time.time()
+def fibonacci(n: int) -> Generator[int,None,None]:
+    yield 0
+    if n > 0: yield
+    last : int = 0
+    next : int = 1
+    for _ in range(1,n):
+        last, next =next, last + next
+        yield next
+
+if __name__ == "__main__":
+    for i in fibonacci(50):
+        print(i)
+print("--- %s seconds ---" % (time.time() - start_time))
 
