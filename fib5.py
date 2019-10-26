66fc99f87957fdda546881e1af28272a52256d62
babichev-oleg <babichev-oleg@mail.ru>
2019-10-26 15:07:45 +0300

    Update fib5.py

--- fib5.py
+++ fib5.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3.7
+import time
+start_time=time.time()
+def fibonacci(n: int) -> int:
+    First_Value = 0
+    Second_Value = 1
+    for Num in range(0, n):
+        if(Num <= 1):
+            Next = Num
+        else:
+            Next = First_Value + Second_Value
+            First_Value = Second_Value
+            Second_Value = Next
+            print(Next)
+
+if __name__ == "__main__":
+    print(fibonacci(10))
+    print(fibonacci(500))
+print("--- %s seconds ---" % (time.time() - start_time))
 
