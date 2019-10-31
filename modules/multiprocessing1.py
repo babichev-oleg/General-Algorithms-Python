e99b331e60cb39d3f97bce8bf22ad32c39c899a7
babichev-oleg <babichev-oleg@mail.ru>
2019-10-31 15:01:06 +0300

    Update multiprocessing1.py

--- modules/multiprocessing1.py
+++ modules/multiprocessing1.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3.6
+import os
+import time
+from multiprocessing import Process
+def doubler(number):
+    result = number ** 2000000000000000000000
+    proc = os.getpid()
+    print('{0} doubled to {1} by process id: {2}'.format(
+        number, result, proc))
+if __name__ == '__main__':
+    numbers = [88888888888, 9999999999, 7777777777, 3333333333, 5555555555]
+    procs = []
+    for index, number in enumerate(numbers):
+        proc = Process(target=doubler, args=(number,))
+        procs.append(proc)
+        proc.start()
+    for proc in procs:
+        proc.join()
 
