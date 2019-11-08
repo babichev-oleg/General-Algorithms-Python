024525b4c12e51af629ac2b0e10cee3bd5f36f43
babichev-oleg <babichev-oleg@mail.ru>
2019-11-08 10:45:32 +0300

    Create fib2.py

---
+++ classic algorithms/fib2.py
@@ -0,0 +1,10 @@
+!/usr/bin/python3.7
+import time
+start_time = time.time()
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+if __name__ == "__main__":
+    print(fibonacci(35))
+print("--- %s seconds ---" % (time.time() - start_time))
