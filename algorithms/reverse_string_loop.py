5043391b39672471d6c75cba64470f6463c18942
babichev-oleg <babichev-oleg@mail.ru>
2019-11-08 10:53:12 +0300

    Create reverse_string_loop.py

---
+++ algorithms/reverse_string_loop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+def reverse(s):
+  str = ""
+  for i in s:
+    str = i + str
+  return str
+
+s = "0123456789"
+if __name__ == "__main__":
+    print ("The original string  is : ",end="")
+    print (s)
+    print ("The reversed string(using loops) is : ",end="")
+    print (reverse(s))
